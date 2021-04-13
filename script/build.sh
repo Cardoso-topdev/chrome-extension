d39cfcc2c9ab8222884c378429949080e60bb4e0
Cardoso-topdev <Oscar@cardoso-devstar.com>
2021-04-13 06:41:47 -0600

    update generate password

--- script/build.sh
+++ script/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-build() {
+build_react() {
     echo 'building react'
 
     rm -rf dist/*
@@ -12,8 +12,40 @@ build() {
 
     mkdir -p dist
     cp -r build/* dist
-
-    mv dist/index.html dist/popup.html
 }
 
-build
+build_others() {
+    echo 'building others'
+
+    # enable webpack(need a webpack.config.js) if you 
+    # want background.js to be minified
+    
+#    webpack
+
+    # background.js and content_script.js should be copied 
+    # to dist if no webpack enabled
+    cp src/background/background.js dist
+    # cp src/content/content_script.js dist
+
+    cp src/background/background.html dist
+#    cp src/background/firebase*.js dist
+#    cp src/background/api.js dist
+    mv dist/index.html dist/popup.html
+    cp dist/popup.html dist/options.html
+    cp public/manifest.json dist/manifest.json
+
+    cp -r public/images dist
+}
+
+if [ $# -eq 0 ]; then
+    echo 'build react|others'
+fi
+
+case $1 in
+'react')
+    build_react
+    ;;
+'others')
+    build_others
+    ;;
+esac
